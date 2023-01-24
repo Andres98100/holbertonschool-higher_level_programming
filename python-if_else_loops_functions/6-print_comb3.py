28f7c882288787182fa3a433447c3b36915a8d2c
andres98100 <andressolarte580@gmail.com>
2023-01-24 16:08:57 -0500

    combination

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", ")
         if i == 8:
             print("{}{}".format(i, j))
-            
+        else:
+            print("{}{}".format(i, j), end=", ")
         
