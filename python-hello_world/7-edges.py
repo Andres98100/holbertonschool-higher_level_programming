7bad57087595122979cf08f59dc04a616a5c6861
andres98100 <andressolarte580@gmail.com>
2023-01-23 19:36:33 -0500

    edges

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_1 = word[:3]
+word_2 = word[-2:]
+word_3 = word[1:-1]
+print(f"First 3 letters: {word_1}")
+print(f"Last 2 letters: {word_2}")
+print(f"Middle word: {word_3}")
