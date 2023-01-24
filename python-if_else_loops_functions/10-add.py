4f7b8f2523b779a3f625d640a3fea07b998818be
andres98100 <andressolarte580@gmail.com>
2023-01-24 16:34:25 -0500

    pow

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -2,8 +2,3 @@
 def add(a, b):
     op = a + b
     return op
-def main():
-    print(add(1, 2))
-    print(add(98, 0))
-    print(add(100, -2))
-main()
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -6,9 +6,3 @@ def print_last_digit(number):
         last = abs(number) % 10
     print("{}".format(last), end="")
     return last
-def main():
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
-main()
