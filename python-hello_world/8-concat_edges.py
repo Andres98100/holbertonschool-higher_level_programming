55f04130072bd5244c8393230b4e9154157261f2
andres98100 <andressolarte580@gmail.com>
2023-01-23 19:49:34 -0500

    new sentence

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with python"
+
+print(str)
