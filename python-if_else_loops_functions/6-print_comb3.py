926b5b08120c4733adc28e2a33033e1c8d5d892b
andres98100 <andressolarte580@gmail.com>
2023-01-24 15:33:24 -0500

    upper lower

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for i in range(0, 100):
+    if i == 99:
+        print(i, end=", ")
+    if (i / 10 and i % 10 == 0) == i:
+        
---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if c >= 97 and c < 132:
+        return True
+    elif c >= 65 and c < 91:
+        return False
