782b480dea56aed17273b49d9e5080ceef3b299d
andres98100 <andressolarte580@gmail.com>
2023-01-25 18:21:41 -0500

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from calculator_1.py import add, subtract, multiply, divide
+from calculator_1.py import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
     result_add = add(a, b)
-    result_substract = subtract(a, b)
-    result_multiply = multiply(a, b)
-    result_divide = divide(a, b)
+    result_substract = sub(a, b)
+    result_multiply = mul(a, b)
+    result_divide = div(a, b)
     print("{} + {} = {}".format(a, b, result_add))
     print("{} - {} = {}".format(a, b, result_substract))
     print("{} * {} = {}".format(a, b, result_multiply))
