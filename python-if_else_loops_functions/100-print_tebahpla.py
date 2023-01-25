b11b3f82a6954fc39a8b2b11a8582ea18a04f57d
andres98100 <andressolarte580@gmail.com>
2023-01-25 12:31:50 -0500

    alphabet revers

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(122, 97 -1, -1):
-    if i % 2 == 0:
+    if i % 2 == 1:
         i -= 32
-    elif i % 1 == 0:
+    elif i % 3 == 0:
         i
     print("{}".format(chr(i)), end="")
+    
