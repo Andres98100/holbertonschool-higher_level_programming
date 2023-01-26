1ed2056065631edeae99353c7100b89d71fa454e
andres98100 <andressolarte580@gmail.com>
2023-01-25 19:08:55 -0500

    argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 import sys
 argv = sys.argv[1:]
 if len(argv) == 0:
-    print("{} arguments.".format(argv))
+    print("{} arguments.".format(len(argv)))
 else:
-    print("{} arguments.".format(argv))
+    if len(argv) == 1:
+        print("{} argument:".format(len(argv)))
+    else:
+        print("{} arguments:".format(len(argv)))
     for i, arg in enumerate(argv, 1):
-        print(i, ":", arg)
+        print("{} : {}".format(i, arg))
