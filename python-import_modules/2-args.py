3b46f015f5ef447bc5492f1015e396d7fd6cc23a
andres98100 <andressolarte580@gmail.com>
2023-01-25 21:54:07 -0500

    hidden

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-	argv = argv[1:]
-	if len(argv) == 0:
-		print("{} arguments.".format(len(argv)))
-	else:
-		if len(argv) == 1:
-			print("{} argument:".format(len(argv)))
-		else:
-			print("{} arguments:".format(len(argv)))
-		for i, arg in enumerate(argv, 1):
-			print("{}: {}".format(i, arg))
+    argv = argv[1:]
+    if len(argv) == 0:
+        print("{} arguments.".format(len(argv)))
+    else:
+        if len(argv) == 1:
+            print("{} argument:".format(len(argv)))
+        else:
+            print("{} arguments:".format(len(argv)))
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4 
 if __name__ == "__main__":
-	for i in dir(hidden_4):
-		if i[:2] != "__":
-			print(i)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
