66ca66ba5e636620102d4302ac85cb730d9048e9
andres98100 <andressolarte580@gmail.com>
2023-01-30 17:42:53 -0500

    max number

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_list = my_list[0] 
+    for i in range(len(my_list)):
+        if my_list[i] > max_list:
+            max_list = my_list[i]
+    return max_list
