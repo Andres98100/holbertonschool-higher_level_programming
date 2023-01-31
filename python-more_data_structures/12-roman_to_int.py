da300769151298317373ef714694330f081cd262
andres98100 <andressolarte580@gmail.com>
2023-01-31 16:16:08 -0500

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string != "" or not roman_string:
+        return 0
     set_roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
     total = 0
     for i in range(len(roman_string) - 1, - 1, -1):
