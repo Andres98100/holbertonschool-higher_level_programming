4f4b980a1f78699a375a058d20841bcf28188604
andres98100 <andressolarte580@gmail.com>
2023-01-31 15:58:39 -0500

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary.keys())
+    aux = 0
+    aux_key = ""
+    for key, value in a_dictionary.items():
+        if value > aux:
+            aux = value
+            aux_key = key
+    return aux_key
