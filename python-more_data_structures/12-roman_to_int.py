dc076d05fd2a1ba118bbb69e848e63bbeada6953
andres98100 <andressolarte580@gmail.com>
2023-01-31 16:18:13 -0500

    update solution

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string != "" or not roman_string:
+    if type(roman_string) != str or not roman_string:
         return 0
     set_roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
     total = 0
