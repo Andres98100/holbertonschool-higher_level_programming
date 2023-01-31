fcf5278966d8c39268a995d79d8a0769477cdf8d
andres98100 <andressolarte580@gmail.com>
2023-01-31 15:12:07 -0500

    update mul

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary in None:
         return
     result = dict(map(lambda x: (x[0], x[1] * 2), a_dictionary.items()))
     return result
