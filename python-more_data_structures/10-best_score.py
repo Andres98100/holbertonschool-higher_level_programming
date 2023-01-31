3dda0c19750d3f5b7492606c03bb1d06b931a3e0
andres98100 <andressolarte580@gmail.com>
2023-01-31 15:33:09 -0500

    multi number

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    result = dict(map(lambda x: (x[0], x[1] < x[1]), a_dictionary.items()))
+    result = dict(map(lambda x: (x[0], max(x[1])), a_dictionary.items()))
     return result
--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    result = list(map(lambda x: (x * number), my_list))
-    return result
+    return list(map(lambda x: (x * number), my_list))
