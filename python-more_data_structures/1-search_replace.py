2c6eaa217503e8c522991866765e7fdcb0682691
andres98100 <andressolarte580@gmail.com>
2023-01-31 12:23:19 -0500

    square matrix and search replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(my_list)):
+        if search == new_list[i]:
+            new_list[i] = replace
+    return new_list
+        
