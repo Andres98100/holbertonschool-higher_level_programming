9f8a08debbea73823e62ec5358ff41f8958cd9f2
andres98100 <andressolarte580@gmail.com>
2023-01-31 14:28:01 -0500

    only elements, numver key and print sorted dic

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    new_key = a_dictionary.keys()
+    new_sorted = sorted(new_key)
+    new_dictionary = {}
+    for i in new_sorted:
+        new_dictionary[i] = a_dictionary[i]
+    print('\n'.join("{}: {}".format(k, v)for k, v in new_dictionary.items()))
+    return new_dictionary
