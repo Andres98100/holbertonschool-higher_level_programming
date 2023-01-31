686f6c0c9e1b50a1aa56d6dcf60473f7f57d0cf9
andres98100 <andressolarte580@gmail.com>
2023-01-31 15:08:43 -0500

    mul

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary == {}:
+        return
+    result = dict(map(lambda x: (x[0], x[1] * 2), a_dictionary.items()))
+    return result
