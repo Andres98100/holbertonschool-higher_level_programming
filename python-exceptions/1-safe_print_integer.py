c65e0a9260ea0c72100a42d716c5f7049def8f2b
andres98100 <andressolarte580@gmail.com>
2023-01-31 19:15:37 -0500

    update print list, print int, print list int, print div

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    safe = int(value)
     try:
         print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
