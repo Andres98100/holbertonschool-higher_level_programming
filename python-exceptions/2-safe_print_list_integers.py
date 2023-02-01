fed2896d4ad3b56d5892e79ecba746ca9613583c
andres98100 <andressolarte580@gmail.com>
2023-01-31 19:21:35 -0500

    update solution print list int

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             print_iterable += 1
         except (TypeError, ValueError):
-            break
+            continue
     print("")
     return print_iterable
