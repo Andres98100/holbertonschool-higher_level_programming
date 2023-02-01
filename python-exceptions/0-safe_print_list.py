f633f52ff15773dfe94a737bbed3874039976ad5
andres98100 <andressolarte580@gmail.com>
2023-01-31 19:12:15 -0500

    print list, print int, print list int, print div

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    print_iterable = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            print_iterable += 1
+        except IndexError:
+            break
+    print("")
+    return print_iterable
---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    print_iterable = 0
+    for i in range(x):
+        try:
+            print("{}".format(int(my_list[i])), end="")
+            iterable += 1
+        except (IndexError, ValueError):
+            break
+    print("")
+    return iterable
