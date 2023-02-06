1bc3b243267e4c6642e85552aa1eefaabdfce1af
andres98100 <andressolarte580@gmail.com>
2023-02-06 15:24:28 -0500

    update solution

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+'''a class called square is initialized'''
+
+
 class Square:
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size=0):
         self.__size = size
+        '''raise an error if the data type is incorrect'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        '''raise an error if the data is less that 0'''
         if size < 0:
             raise ValueError("size must be >= 0")
+    '''returns the current square area'''
     def area(self):
         return self.__size * self.__size
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-'''
-an empty class is initialized
-'''
+'''a class called square is initialized'''
+
+
 class Square:
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size):
         self.__size = size
+    '''private instance of the attribute is returned'''
     def get_size(self):
         return self.__size
     def size(self, value):
+        '''raise an error if the data type is incorrect'''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        '''raise an error if the data is less that 0'''
         if value < 0:
             raise ValueError("size must be >= 0")
+        '''is instantiated with the value'''
         self.__size = value
+    '''returns the current square area'''
     def area(self):
         return self.__size * self.__size
