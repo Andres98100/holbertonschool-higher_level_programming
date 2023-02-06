dfe2679af5b2bcf3366ccce3435725c09f83eb8f
andres98100 <andressolarte580@gmail.com>
2023-02-06 14:41:08 -0500

    square init, square private, square raise msg, square area and square area in size

---
+++ python-classes/4-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        self.__size = size
+    def get_size(self):
+        return self.__size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        return self.__size * self.__size
