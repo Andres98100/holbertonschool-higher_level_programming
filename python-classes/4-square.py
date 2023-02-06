90cda4e7bf3682dbece6668a8806be12ee85fa81
andres98100 <andressolarte580@gmail.com>
2023-02-06 14:57:46 -0500

    update readme

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,4 +11,4 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
-        return self.__size * self.__size 
+        return self.__size * self.__size
