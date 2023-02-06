50a7071b2c39e0d81681c77121ad85c969443d25
andres98100 <andressolarte580@gmail.com>
2023-02-06 15:01:49 -0500

    update readme

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+'''
+an empty class is initialized
+'''
 class Square:
     pass
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+an empty class is initialized
+'''
 class Square:
     def __init__(self, size):
         self.__size = size
