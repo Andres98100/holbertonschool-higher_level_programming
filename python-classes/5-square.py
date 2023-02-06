8d698c749e19f466b89c2903a69a91a1af922942
andres98100 <andressolarte580@gmail.com>
2023-02-06 17:58:10 -0500

    print #

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+'''a class called square is initialized'''
+
+
+
 class Square:
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size=0):
         self.__size = size
+    '''private instance of the attribute is returned'''
     @property
     def get_size(self):
         return self.__size
     @get_size.setter
     def size(self, value):
+        '''raise an error if the data type is incorrect'''
         if not isinstance(value, int):
-            print("size must be an intege")
+            raise TypeError("size must be an intege")
+        '''raise an error if the data is less that 0'''
         if value < 0:
-            print("size must be >= 0")
+            raise ValueError("size must be >= 0")
+        '''is instantiated with the value'''
         self.__size = value
+    '''returns the current square area'''
     def area(self):
-        return self.__size 
-    
+        return self.__size * self.__size
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
