bdbe5788a4a5c1dfedf8e41f9272cd2828fcdfe8
andres98100 <andressolarte580@gmail.com>
2023-02-06 15:05:23 -0500

    update readme

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''an empty class called square is initialized'''
+
+
 class Square:
     '''the keyword 'pass' is used to indicate
     that the class will have no instructions'''
