0651c5b7441772170a20b5e303cd749e89a5e275
andres98100 <andressolarte580@gmail.com>
2023-02-06 15:14:09 -0500

    update solution

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''a class called square is initialized'''
+
+
 class Square:
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size):
         self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+'''a class called square is initialized'''
+
+
 class Square:
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size = 0):
         self.__size = size
+        '''raise an error if the data type is incorrect'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        '''raise an error if the data is less that 0'''
         if size < 0:
             raise ValueError("size must be >= 0")
