73af4e5c3c31a62d6df61a482671086b78439aed
andres98100 <andressolarte580@gmail.com>
2023-02-07 10:13:18 -0500

    update solution

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
     @get_position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(i, int)and i >= 0 for i in value):
+           not all(i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -46,6 +46,6 @@ class Square:
             '''is iterated size times and then
             multiplied by the size to create the'''
             for i in range(self.__position[1]):
-                print("")
+                print()
             for i in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
