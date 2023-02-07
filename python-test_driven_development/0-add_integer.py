f1d3abb6627991bc051a22640d6f296e858a8e96
andres98100 <andressolarte580@gmail.com>
2023-02-07 15:24:32 -0500

    test v.1

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
---
+++ python-test_driven_development/test.txt/0-add_integer.txt
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
