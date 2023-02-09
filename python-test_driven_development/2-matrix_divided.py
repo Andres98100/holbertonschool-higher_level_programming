cb3edbc6f7fe9555a1a474bc0c3b05b20dbcfb5c
andres98100 <andressolarte580@gmail.com>
2023-02-09 13:46:36 -0500

    update v1.0

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,18 +3,14 @@
 
 
 def matrix_divided(matrix, div):
-    '''validates that the lists of the array are int or float'''
-    if not all(list(map(lambda x: isinstance(x, int) and (x, float), matrix))):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     aux = len(matrix[0])
-    '''it is validated that the size of the lists are the same'''
-    if not all(list(map(lambda x: len(x) == aux, matrix))):
+    if not all(list(map(lambda x: all(list(map(lambda num: isinstance(num, (int, float)), x))), matrix))):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    elif not all(list(map(lambda x: len(x) == aux, matrix))):
         raise TypeError("Each row of the matrix must have the same size")
-    '''validates that div is int or float'''
-    if not isinstance(div, int) and isinstance(div, float):
+    elif not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    '''div different to zero'''
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
     result = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
     return result
