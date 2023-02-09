8e22e4329b288330362f87e4fc4ea368e4010a9a
andres98100 <andressolarte580@gmail.com>
2023-02-09 13:54:33 -0500

    update v1.3

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,9 +4,9 @@
 
 def matrix_divided(matrix, div):
     aux = len(matrix[0])
-    '''it is validated that the matrix is int or float and that div is not zero.'''
-    if not all(list(map(lambda x: all(list(map(lambda num: 
-            isinstance(num, (int, float)), x))), matrix))):
+    '''matrix is int or float'''
+    if not all(list(map(lambda x: all(list(map(lambda num:
+        isinstance(num, (int, float)), x))), matrix))):
         '''error of the matrix'''
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     elif not all(list(map(lambda x: len(x) == aux, matrix))):
@@ -15,6 +15,7 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
-    result = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    result = list(map(lambda x: list(map(lambda y:
+        round(y / div, 2), x)), matrix))
     '''return the result'''
     return result
