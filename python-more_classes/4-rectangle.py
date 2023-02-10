75393eae4fc6941beb00b89982e2e10a171d032f
andres98100 <andressolarte580@gmail.com>
2023-02-09 19:02:58 -0500

    more classes

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rectangle += "#"
-            if i < self.__height - 1:
+            if i != self.__height - 1:
                 rectangle += '\n'
         '''return rectangle'''
         return rectangle
