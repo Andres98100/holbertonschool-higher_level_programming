cd200a50b150a4f69c728f13e53147109ca22abc
andres98100 <andressolarte580@gmail.com>
2023-02-14 11:07:51 -0500

    update txt

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,9 +7,11 @@ class BaseGeometry:
     def area(self):
         '''exception'''
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         '''validation'''
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        return value
---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''class basegeometry'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'''class rectangle inherits the class basegeometry'''
+
+
+class Rectangle(BaseGeometry):
+    '''method'''
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+    '''method'''
+    def area(self):
+        '''return area'''
+        return self.__width * self.__height
+    '''method'''
+    def __str__(self):
+        '''return print str'''
+        return print("[Rectangle] {}/{}".format(self.__width, self.__height))
