dcf005e3e384e564c287a8f2ddb11f39127bd895
andres98100 <andressolarte580@gmail.com>
2023-02-14 14:37:36 -0500

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,8 +4,6 @@
 
 def read_file(filename=""):
     '''statement'''
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         '''read lines'''
-        for line in file:
-            print(line, end="")
-    print()
+        print(file.read(), end="")
