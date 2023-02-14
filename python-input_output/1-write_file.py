6f01ce32173df3eaf6efcd824e717b1954c9d90c
andres98100 <andressolarte580@gmail.com>
2023-02-14 15:22:05 -0500

    read write append txt

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-''''''
+'''function write in a file txt'''
 
 
 def write_file(filename="", text=""):
+    '''statement'''
     with open(filename, "w", encoding="utf-8") as file:
         file.write
+    '''return'''
     return len(text)
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-''''''
+'''function append text a in file txt'''
 
 
 def append_write(filename="", text=""):
+    '''statement'''
     with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
+    '''return'''
     return len(text)
