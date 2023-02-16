89c21dddfcc8d31e952956ad430de91a0316cc83
andres98100 <andressolarte580@gmail.com>
2023-02-16 16:56:05 -0500

    student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''class'''
+
+
+class Student:
+    '''method'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    '''function'''
+    def to_json(self, attrs=None):
+        '''validation'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            '''iterates the list'''
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            '''return'''
+            return new_dict
+    '''function'''
+    def reload_from_json(self, json):
+        '''validation'''
+        if json:
+            new_dict = self.__dict__
+        '''iterates the attr'''
+        for i in new_dict.keys():
+            new_dict[i] = json[i]
--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-''''''
+'''function crated a pascal triangle'''
+
+
 def pascal_triangle(n):
+    """Represents a Pascal's triangle of n"""
     if n <= 0:
-        return n
-    
+        return []
+    triangle = [[1 for column in range(row)] for row in range(1, n + 1)]
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+    return triangle
