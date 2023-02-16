6c3b3ec6fd97d4538a0415d23c1ea4a14d89b460
andres98100 <andressolarte580@gmail.com>
2023-02-16 15:00:49 -0500

    update add_item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,19 +11,22 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv
 filename = 'add_item.json'
 '''function'''
+
+
 def add_list():
     '''variable'''
     obj_list = []
     '''validation'''
-    if os.stat(filename).st_size == 0:
-        obj_list
-    elif os.path.exists(filename):
-        obj_list = load_from_json_file(filename)
-        
-    '''add args'''
-    obj_list.extend(args[1:])
-    '''save list'''
-    save_to_json_file(obj_list, filename)
+    with open(filename, "w") as file:
+        if os.stat(filename).st_size == 0:
+            obj_list
+        elif os.path.exists(filename):
+            obj_list = load_from_json_file(filename)
+            
+        '''add args'''
+        obj_list.extend(args[1:])
+        '''save list'''
+        save_to_json_file(obj_list, filename)
 '''function'''
 if __name__ == "__main__":
     add_list()
