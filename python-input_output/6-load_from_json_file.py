87bed28536a5ddb94fcdc6cee2f047a3c5481e97
andres98100 <andressolarte580@gmail.com>
2023-02-16 11:31:48 -0500

    update load to json

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,9 @@ import json
 
 
 def load_from_json_file(filename):
+    '''open json'''
     with open(filename, "r") as file:
+        '''load json'''
         data = json.loads(file.read())
+    '''return'''
     return data
