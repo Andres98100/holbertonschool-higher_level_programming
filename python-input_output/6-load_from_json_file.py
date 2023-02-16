9e3a2b32f2137751bacf69fc770894cd5d61d9d8
andres98100 <andressolarte580@gmail.com>
2023-02-16 11:27:35 -0500

    load to json

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 '''import'''
 import json
-'''function'''
+'''function  creates an Object from a json'''
 
 
 def load_from_json_file(filename):
-    with open(filename) as file:
-        
+    with open(filename, "r") as file:
+        data = json.loads(file.read())
+    return data
