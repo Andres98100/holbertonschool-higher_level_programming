7007cc94226e91f115b72d37fb033b7da18e374b
andres98100 <andressolarte580@gmail.com>
2023-02-16 12:09:19 -0500

    add list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,21 +4,23 @@ import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-'''variables'''
-args = sys.argv
-my_list = []
-filename = "add_item.json"
 '''function add list'''
 
 
 
 def add_list():
+    '''variables'''
+    args = sys.argv
+    my_list = []
+    filename = "add_item.json"
     '''open json'''
     with open(filename, 'r') as f:
         '''my list'''
         my_list = []
         '''add my list'''
         my_list.extend(args[1:])
+        '''save in the json'''
         save_to_json_file(my_list, filename)
         load_from_json_file(filename)
+'''function'''
 add_list()
