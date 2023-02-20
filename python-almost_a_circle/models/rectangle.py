9e6765500d8e9795ef754abe4a926b471f0d7981
andres98100 <andressolarte580@gmail.com>
2023-02-20 13:20:00 -0500

    rectangle area, display and method str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,5 @@ class Rectangle(Base):
     def __str__(self):
         '''return'''
         return ("[Rectangle] ({}) {}/{} - {}/{}"
-                .format(self.id, self.__x, self.__y, 
+                .format(self.id, self.__x, self.__y,
                         self.__width, self.__height))
