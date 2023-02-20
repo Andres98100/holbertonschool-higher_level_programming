377bf58ae756ce4f3c165dcc0e625217435ecb5b
andres98100 <andressolarte580@gmail.com>
2023-02-20 11:31:03 -0500

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
     '''method constructor'''
     def __init__(self, id=None):
         '''conditional'''
-        if not id is None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
