a68146bb97053e5c93856ea32c26b2c1a6be4f96
andres98100 <andressolarte580@gmail.com>
2023-02-20 12:47:48 -0500

    validations Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,43 +12,67 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-    '''decorators'''
+
     @property
     def width(self):
         '''return'''
         return self.__width
-    '''decorators'''
+
     @width.setter
     def width(self, value):
+        '''validation'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        '''validation'''
+        if value <= 0:
+            raise ValueError("width must be > 0")
         '''assignment'''
         self.__width = value
-    '''decorators'''
+
     @property
     def height(self):
         '''return'''
         return self.__height
-    '''decorators'''
+
     @height.setter
     def height(self, value):
+        '''validation'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        '''validation'''
+        if value <= 0:
+            raise ValueError("height must be > 0")
         '''assignment'''
         self.__height = value
-    '''decorators'''
+
     @property
     def x(self):
         '''return'''
         return self.__x
-    '''decorators'''
+
     @x.setter
     def x(self, value):
+        '''validation'''
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        '''validation'''
+        if value < 0:
+            raise ValueError("x must be > 0")
         '''assignment'''
         self.__x = value
-    '''decorators'''
+
     @property
     def y(self):
         '''return'''
         return self.__y
-    '''decorators'''
+
     @y.setter
     def y(self, value):
+        '''validation'''
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        '''validation'''
+        if value < 0:
+            raise ValueError("y must be > 0")
         '''assignment'''
         self.__y = value
