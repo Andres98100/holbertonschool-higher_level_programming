ebc76bc8dfbc600e11369a15b286f267273b6b72
andres98100 <andressolarte580@gmail.com>
2023-02-20 11:26:16 -0500

    base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''class'''
+
+
+class Base:
+    '''private attribute'''
+    __nb_objects = 0
+    '''method constructor'''
+    def __init__(self, id=None):
+        '''conditional'''
+        if not id == None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
