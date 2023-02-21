b3fd5fc49f43e4e2e1b9a0dbb9f0ef757b9855b0
andres98100 <andressolarte580@gmail.com>
2023-02-21 13:28:53 -0500

    update square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     @property
     def size(self):
         '''return'''
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -23,15 +23,14 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError("width must be > 0")
         '''assignment'''
-        self.__size = value
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         '''return'''
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y,
-                        self.__width))
+                        self.width))
 
     '''method assignment'''
     def update(self, *args, **kwargs):
@@ -41,7 +40,7 @@ class Square(Rectangle):
             if i == 0:
                 self.id = args[i]
             elif i == 1:
-                self.__size = args[i]
+                self.width = args[i]
             elif i == 2:
                 self.x = args[i]
             elif i == 3:
@@ -53,7 +52,7 @@ class Square(Rectangle):
             if key == "id":
                 self.id = value
             elif key == "size":
-                self.__size = value
+                self.size = value
             elif key == "x":
                 self.x = value
             elif key == "y":
