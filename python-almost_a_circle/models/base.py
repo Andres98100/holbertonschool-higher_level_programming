fb3e83cbd5037f916d34fc7f5ee46f945f17e387
andres98100 <andressolarte580@gmail.com>
2023-02-23 12:38:18 -0500

    save and from

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-             return json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
