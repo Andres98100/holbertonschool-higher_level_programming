c7651556df00b5c0556260fe9c74b334231dbb11
andres98100 <andressolarte580@gmail.com>
2023-02-23 12:36:57 -0500

    save and frmo

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''import'''
+import json
+import os.path
 '''class'''
 
 
@@ -13,3 +16,35 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''validation'''
+        if list_objs is None:
+            list_objs = []
+        '''variable'''
+        filename = cls.__name__ + ".json"
+        new_list = []
+        '''open json'''
+        with open(filename, "w") as file:
+            '''iterates list_objs'''
+            for i in list_objs:
+                new_list.append(i.to_dictionary())
+            '''write in the file json'''
+            data = Base.to_json_string(new_list)
+            file.write(data)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''validation'''
+        if json_string is None or len(json_string) == 0:
+            return []
+        '''return'''
+        return json.loads(json_string)
