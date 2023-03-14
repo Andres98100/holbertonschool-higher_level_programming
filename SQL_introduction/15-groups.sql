2d0703ce3ad687343910dfa1f0a2f2b6ed023ea5
Andres98100 <andressolarte580@gmail.com>
2023-03-13 20:11:24 -0500

    same number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- lists the number of records with the same score
-SELECT COUNT(score) AS number FROM second_table WHERE score = score ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score
+ORDER BY number DESC;
