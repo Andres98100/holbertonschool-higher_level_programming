4f8198402c8c6e17197aab6a7e748680e9646989
Andres98100 <andressolarte580@gmail.com>
2023-03-13 20:28:24 -0500

    update

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- creates a new table with new values
-    CREATE TABLE IF NOT EXISTS second_table(
-        id INT,
-        name VARCHAR(56),
-        score INT
-    );
-    INSERT INTO second_table (id, name, score) VALUES
-        (1, 'Jhon', 10),
-        (2, 'Alex', 3),
-        (3, 'Bob', 14),
-        (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table(
+id INT,
+name VARCHAR(56),
+score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'Jhon', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
