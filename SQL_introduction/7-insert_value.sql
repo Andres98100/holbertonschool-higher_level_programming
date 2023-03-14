4014e47306b9292d465182d1375231182e8fb656
Andres98100 <andressolarte580@gmail.com>
2023-03-13 19:15:41 -0500

    second table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- insert a new row to table
-INSERT INTO first_table (id, name) VALUE (89, 'BestSchool');
+INSERT INTO first_table (id, name) VALUE (89, 'Best School');
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- 
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(56),
+    score INT
+);
+INSERT INTO second_table (id, name ,score) VALUE
+    (1, 'Jhon', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
