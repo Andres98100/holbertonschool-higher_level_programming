eeb6f18271b976c2a89cc12900b39934c3e591c4
Andres98100 <andressolarte580@gmail.com>
2023-03-14 18:29:33 -0500

    foregin and reference

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-    id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
-    state_id NOT NULL,
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
