0894024f27009671c859b23f03b82565147092e8
Andres98100 <andressolarte580@gmail.com>
2023-03-14 19:00:21 -0500

    california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --  lists all the cities of California
-SELECT * FROM hbtn_0d_usa.cities WERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
+SELECT * FROM hbtn_0d_usa.cities WhERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
