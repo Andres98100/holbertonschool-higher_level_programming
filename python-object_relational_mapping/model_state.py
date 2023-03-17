1a5d7def404ee17a1f35e146e712ab093fb23d3a
Andres98100 <andressolarte580@gmail.com>
2023-03-17 13:42:33 -0500

    update cities and tables

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,6 +11,6 @@ Base = declarative_base()
 class State(Base):
     """ table declaration """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key = True, unique = True,
+    id = Column(Integer, primary_key=True, unique=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128))
