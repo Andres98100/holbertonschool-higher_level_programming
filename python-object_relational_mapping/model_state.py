39468a921c8d10dcbd042485c45e8a77010d4fd2
Andres98100 <andressolarte580@gmail.com>
2023-03-17 13:41:24 -0500

    update cities and tables

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-create table 
+create table
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ table declaration """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key = True,
-                unique = True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key = True, unique = True,
+                autoincrement=True, nullable=False)
     name = Column(String(128))
