84eb9d392422449d825b97f667634bf0be1c86a0
Andres98100 <andressolarte580@gmail.com>
2023-03-18 09:59:37 -0500

    filter and states

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).filter(State.name.like('%a%')).all()
+    states = session.query(State).order_by(State.id).\
+        filter(State.name.like('%a%')).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
