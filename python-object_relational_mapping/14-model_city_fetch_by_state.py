935cd5eecf6411ee9e6ed946995f7f45e0f156ca
Andres98100 <andressolarte580@gmail.com>
2023-03-18 11:34:16 -0500

    model city finish

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+..
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    cities = session.query(City).order_by(City.id).all()
+    
+    for city in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    session.close()
+    
---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128))
+    state_id = Column(Integer,
+                      ForeignKey('states.id'))
+    state = relationship('State')
