f71cfff0a12e04f9ef84ec0a9f22581948308660
Andres98100 <andressolarte580@gmail.com>
2023-04-14 19:36:29 -0500

    update

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-const process = require('process');
-const args = process.argv;
-let maxNumber = args[2];
-let secNumber = args[2];
-
-if (args.length > 2) {
-  for (let index = 0; index < args.length - 2; index++) {
-    let n = parseInt(args[index]);
-    if (maxNumber < n) {
-      secNumber = maxNumber;
-      maxNumber = n;
-    } else if (n > secNumber) {
-      secNumber = n;
+const args = process.argv.slice(2);
+if (args.length < 2) {
+  console.log(0);
+} else {
+  let biggest = Number.MIN_SAFE_INTEGER;
+  let secBiggest = Number.MIN_SAFE_INTEGER;
+  for (let i = 0; i < args.length; i++) {
+    const num = parseInt(args[i]);
+    if (num > biggest) {
+      secBiggest = biggest;
+      biggest = num;
+    } else if (num > secBiggest) {
+      secBiggest = num;
     }
   }
+  console.log(secBiggest);
 }
-console.log(secNumber);
