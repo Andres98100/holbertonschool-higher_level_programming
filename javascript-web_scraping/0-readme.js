8e83cde81813883bbb39a7e096dcb4934f9b2f4e
Andres98100 <andressolarte580@gmail.com>
2023-04-19 19:22:12 -0500

    files and api

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readPath(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   }
